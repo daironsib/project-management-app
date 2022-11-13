2095083af4d84051df9f8a9ff7592ad6fd2c0a8f
AlenaDoz <alena.abunassar1@mail.ru>
2022-11-13 19:24:36 +0300

    feat: add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+const HeaderBlock = styled.div`
+width: 100%;
+height: 70px;
+background-color: green;
+display: flex;
+padding-left: 20px;
+`
+const SwitcherLabel = styled.label`
+display: block;
+position: relative;
+background-color: white;
+width: 20px;
+height: 15px;
+transition: all 0.2s ease-in;
+&:after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  width: 5px;
+  height: 5px;
+  border-radius: 50%;
+  background-color: blue;
+  transition: all 0.3s ease-out;
+}
+`
+const SwitcherInput = styled.input`
+display: none;
+&:checked + ${SwitcherLabel} {
+background-color: pink;
+}
+&:checked + ${SwitcherLabel}::after {
+left: 13px;
+}
+`
+const NavBlock = styled.div`
+  margin: 20px;
+  margin-left: auto;
+  display: flex;
+  gap: 15px;
+`
+const SwitcherBlock = styled.div`
+display: flex;
+margin: 20px;
+gap: 5px;
+`
+const Header = () => {
+  const [ langs, setLangs ] = useState('EN');
+  const [ auth, setAuth ] = useState(true);
+  const changeHandler = () => {
+    setLangs(langs === 'EN'?'RU':'EN');
+  }
+  return (
+    <HeaderBlock>
+      <NavLink to='/'>Logo</NavLink>
+      <NavBlock>{auth?(<><NavLink to='/sign-in'>Sign in</NavLink>
+      <NavLink to='/sign-up'>Sign-up</NavLink></>):(<>
+      <NavLink to='/edit-profile'>edit profile</NavLink>
+      <NavLink to='/create-board'>create board</NavLink>
+      <NavLink to='/sign-out'>sign out</NavLink>
+      </>)}</NavBlock>
+      <SwitcherBlock>
+        <span>RU</span>
+        <SwitcherInput type="checkbox" id="language-switcher" onChange={changeHandler}/>
+        <SwitcherLabel htmlFor="language-switcher"></SwitcherLabel>
+        <span>EN</span>
+      </SwitcherBlock>
+    </HeaderBlock>
+  )
+}
+
+export default Header;
