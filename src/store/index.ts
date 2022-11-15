7a14ecd5d361e7ffbc148a5f36ac0ba75f6a2eee
Dairon <daironsib@gmail.com>
2022-11-15 12:03:47 +0100

    feat: add redux

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export type AppDispatch = typeof store.dispatch;
