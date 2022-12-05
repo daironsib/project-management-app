90e44a46e91467bc6e1a66dbc1b7ecaca3727357
rybakovmax23 <rybakovmax23@gmail.com>
2022-12-06 00:27:58 +0300

    feat: add update taskDeatails for title

--- src/components/TaskDetails/TaskDetails.tsx
+++ src/components/TaskDetails/TaskDetails.tsx
@@ -4,7 +4,13 @@ import {
   TaskDetailsModal,
   TaskDetailsTitle,
 } from './style';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { EditableTitle } from '../EditableTitle/EditableTitle';
+import { useCallback } from 'react';
+import { IAddEditModal } from '../../types/interfaces';
+import { updateTask } from '../../store/tasksSlice/tasksActions';
+import { useParams } from 'react-router-dom';
+import { setTaskDetails } from '../../store/tasksSlice/tasksSlice';
 
 interface IProps {
   isOpened: boolean;
@@ -13,10 +19,40 @@ interface IProps {
 
 const DeleteModal = ({ isOpened, closeModal }: IProps) => {
   const { taskDetails } = useAppSelector((state) => state.tasks);
+  const dispatch = useAppDispatch();
+  const boardId = useParams().id;
+  console.log(taskDetails);
 
+  const taskTitle = taskDetails?.title as string;
   const handleCancelClick = () => {
     closeModal();
   };
+
+  const updateТaskHandler = useCallback(
+    ({ title }: IAddEditModal) => {
+      if (taskDetails && boardId) {
+        const { columnId, _id, order, userId, users } = taskDetails;
+        const data = {
+          title: title ? title : taskDetails.title,
+          order: order,
+          description: taskDetails.description,
+          columnId,
+          userId,
+          users,
+        };
+        dispatch(setTaskDetails(data));
+        dispatch(
+          updateTask({
+            boardId,
+            columnId,
+            taskId: _id,
+            data,
+          })
+        );
+      }
+    },
+    [boardId, dispatch, taskDetails]
+  );
 
   return (
     <Overlay
@@ -25,8 +61,12 @@ const DeleteModal = ({ isOpened, closeModal }: IProps) => {
       disableAutoFocus={true}
     >
       <TaskDetailsModal>
-        <TaskDetailsTitle>{ taskDetails?.title }</TaskDetailsTitle>
-        <TaskDetailsDescription>{ taskDetails?.description }</TaskDetailsDescription>
+        <TaskDetailsTitle>
+          <EditableTitle title={taskTitle} dispatch={updateТaskHandler} />
+        </TaskDetailsTitle>
+        <TaskDetailsDescription>
+          {taskDetails?.description}
+        </TaskDetailsDescription>
       </TaskDetailsModal>
     </Overlay>
   );
